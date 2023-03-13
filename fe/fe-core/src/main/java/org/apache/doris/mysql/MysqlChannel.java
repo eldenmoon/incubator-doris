0972bdb69e447b952202c891790f4a2efd08325d
eldenmoon <15605149486@163.com>
2023-03-13 14:11:27 +0800

    [Feature](PreparedStatement) implement general server side prepared

--- fe/fe-core/src/main/java/org/apache/doris/mysql/MysqlChannel.java
+++ fe/fe-core/src/main/java/org/apache/doris/mysql/MysqlChannel.java
@@ -59,8 +59,29 @@ public class MysqlChannel {
     // Serializer used to pack MySQL packet.
     protected volatile MysqlSerializer serializer;
 
+    // mysql useServerPrepStmts
+    private boolean useServerPrepStmts;
+    // mysql flag CLIENT_DEPRECATE_EOF
+    private boolean clientDeprecatedEOF;
+
     protected MysqlChannel() {
         // For DummyMysqlChannel
+    }
+
+    public void setUseServerPrepStmts() {
+        useServerPrepStmts = true;
+    }
+
+    public void setClientDeprecatedEOF() {
+        clientDeprecatedEOF = true;
+    }
+
+    public boolean useServerPrepStmts() {
+        return useServerPrepStmts;
+    }
+
+    public boolean clientDeprecatedEOF() {
+        return clientDeprecatedEOF;
     }
 
     public MysqlChannel(StreamConnection connection) {
