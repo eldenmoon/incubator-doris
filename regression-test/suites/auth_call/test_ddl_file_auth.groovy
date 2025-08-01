c26b8f78d2a8ddcca6a9f6dab02b22e7711c1114
chunping <zhangchunping@selectdb.com>
2025-08-01 16:24:12 +0800

    [test](fix) grant some auth case at right places (#4238)

--- regression-test/suites/auth_call/test_ddl_file_auth.groovy
+++ regression-test/suites/auth_call/test_ddl_file_auth.groovy
@@ -18,23 +18,26 @@
 import org.junit.Assert;
 
 suite("test_ddl_file_auth","p0,auth_call") {
+    def authComputeToUser = { String user ->
+        //cloud-mode
+        if (isCloudMode()) {
+            def clusters = sql " SHOW CLUSTERS; "
+            assertTrue(!clusters.isEmpty())
+            def validCluster = clusters[0][0]
+            sql """GRANT USAGE_PRIV ON CLUSTER `${validCluster}` TO ${user}""";
+        }
+    }
+
     String user = 'test_ddl_file_auth_user'
     String pwd = 'C123_567p'
     String dbName = 'test_ddl_file_auth_db'
     String fileName = 'test_ddl_file_auth_file'
 
-    //cloud-mode
-    if (isCloudMode()) {
-        def clusters = sql " SHOW CLUSTERS; "
-        assertTrue(!clusters.isEmpty())
-        def validCluster = clusters[0][0]
-        sql """GRANT USAGE_PRIV ON CLUSTER `${validCluster}` TO ${user}""";
-    }
-
     try_sql("DROP USER ${user}")
     try_sql """drop database if exists ${dbName}"""
     sql """CREATE USER '${user}' IDENTIFIED BY '${pwd}'"""
     sql """grant select_priv on regression_test to ${user}"""
+    authComputeToUser(user)
     sql """create database ${dbName}"""
 
     String s3_endpoint = getS3Endpoint()
--- regression-test/suites/auth_call/test_dml_insert_auth.groovy
+++ regression-test/suites/auth_call/test_dml_insert_auth.groovy
@@ -19,6 +19,15 @@ import org.junit.Assert;
 import org.codehaus.groovy.runtime.IOGroovyMethods
 
 suite("test_dml_insert_auth","p0,auth_call") {
+    def authComputeToUser = { String user ->
+        //cloud-mode
+        if (isCloudMode()) {
+            def clusters = sql " SHOW CLUSTERS; "
+            assertTrue(!clusters.isEmpty())
+            def validCluster = clusters[0][0]
+            sql """GRANT USAGE_PRIV ON CLUSTER `${validCluster}` TO ${user}""";
+        }
+    }
 
     String user = 'test_dml_insert_auth_user'
     String pwd = 'C123_567p'
@@ -26,18 +35,11 @@ suite("test_dml_insert_auth","p0,auth_call") {
     String tableName = 'test_dml_insert_auth_tb'
     String srcTableName = 'test_dml_insert_auth_tb_src'
 
-    //cloud-mode
-    if (isCloudMode()) {
-        def clusters = sql " SHOW CLUSTERS; "
-        assertTrue(!clusters.isEmpty())
-        def validCluster = clusters[0][0]
-        sql """GRANT USAGE_PRIV ON CLUSTER `${validCluster}` TO ${user}""";
-    }
-
     try_sql("DROP USER ${user}")
     try_sql """drop database if exists ${dbName}"""
     sql """CREATE USER '${user}' IDENTIFIED BY '${pwd}'"""
     sql """grant select_priv on regression_test to ${user}"""
+    authComputeToUser(user)
     sql """create database ${dbName}"""
 
     sql """create table ${dbName}.${tableName} (
